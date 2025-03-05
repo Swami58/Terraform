29d9f450c6d5f3899a2781298c1a884f47e65534
swami <lakamanaswathi2021.com>
2025-03-05 15:24:30 +0530

    ec2

---
+++ ec2/ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allow ssh"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_block  = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "Swami"
+    }
+
+}
---
+++ ec2/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hasicorp/aws"
+        version = "5.87.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1                                                                                                                                                                                                                                                                                                                                                                   jm "
+  
+}
