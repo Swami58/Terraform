1411d4374430f901278a368dc6a82e5e851b9c0b
swami <lakamanaswathi2021.com>
2025-03-10 11:34:01 +0530

    ec2

---
+++ count/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "db" {
+    count = 3
+    ami ="ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    tags = {
+        Name = "db"
+    }
+}
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allow ssh"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "Swami"
+    }
+}
